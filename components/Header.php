feaf2314b74f467677ac6496dfb7d266b2b6165e
lirgen <lirgenbodinaku@gmail.com>
2023-04-03 19:17:15 +0200

    Header

---
+++ components/Header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <header class="page-section compact-header">
+        <nav class="masthead">
+            <div class="inner-w"><a href="https://www.dreamworks.com" data-destination="home" class="home-link">
+                    <figure class="logo"><i class="svg-icon dw-logo"><svg class="icon-dw-logo">
+                                <use xlink:href="#icon-dw-logo"></use>
+                            </svg></i></figure>
+                </a>
+                <div class="small-screen-controls"><button data-sub-menu-open="search" role="button" aria-label="search" class="switch search"><i class="svg-icon search"><svg class="icon-search">
+                                <use xlink:href="#icon-search"></use>
+                            </svg></i></button>
+                    <div role="button" data-sub-menu="toggle" class="switch sub-menu-toggle active">
+                        <div class="icon">
+                            <div class="line one"></div>
+                            <div class="line two"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav> <!---->
+    </header>
+</head>
+
+<body>
+
+</body>
+
+</html>
